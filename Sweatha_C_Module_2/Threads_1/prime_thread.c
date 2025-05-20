597f67b95b1ec539929aff51e97922014740fb00
Sweatha1110 <sweatharam11@gmail.com>
2025-05-21 00:09:18 +0530

    module_2.1

---
+++ Sweatha_C_Module_2/Threads_1/prime_thread.c
@@ -0,0 +1,98 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+#include <pthread.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <time.h>
+#include <stdlib.h>
+
+
+int g_N;
+
+bool is_prime(int n)
+{
+    if(n<2)
+    {
+        return false;
+    }
+    else
+    {
+        for(int i=2;i*i<=n;i++)
+        {
+            if(n%i==0)
+            {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+void* A_func_thread(void* arg)
+{
+    int n=2,sum=0,c=0;
+    
+    while(c<g_N)
+    {
+       if(is_prime(n))
+       {
+           sum+=n;
+           c++;
+       }
+       n++;
+    }
+    
+    printf("The sum of first %d prime numbers : %d\n",g_N,sum);
+    return NULL;
+}
+
+void* B_func_thread(void* arg)
+{
+    time_t start_time = time(NULL);
+    while(difftime(time(NULL),start_time)<100)
+    {
+        printf("Thread B is running successfully\n");
+        sleep(2);
+    }
+    return NULL;
+    
+}
+
+void* C_func_thread(void* arg)
+{
+    time_t start_time = time(NULL);
+    while(difftime(time(NULL),start_time)<100)
+    {
+        printf("Thread C is running successfully\n");
+        sleep(3);
+    }
+    return NULL;
+    
+}
+
+int main()
+{
+    pthread_t tA,tB,tC;
+    
+    printf("Enter the value of N ");
+    scanf("%d",&g_N);
+    
+    
+    pthread_create(&tA,NULL,A_func_thread,NULL);
+    pthread_create(&tB,NULL,B_func_thread,NULL);
+    pthread_create(&tC,NULL,C_func_thread,NULL);
+    
+    
+    pthread_join(tA,NULL);
+    pthread_join(tB,NULL);
+    pthread_join(tC,NULL);
+    
+    return 0;
+}
---
+++ Sweatha_C_Module_2/Threads_2/signal.c
@@ -0,0 +1,112 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+#include <signal.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <time.h>
+#include <stdlib.h>
+
+
+int g_N;
+
+/*
+
+handle_int function to prevent 
+abrupt termination of process 
+when ctrl+c is entered by the user
+
+**/
+
+void handle_int(int sig)
+{
+    printf("signal %d caught\n",sig);
+}
+
+bool is_prime(int n)
+{
+    if(n<2)
+    {
+        return false;
+    }
+    else
+    {
+        for(int i=2;i*i<=n;i++)
+        {
+            if(n%i==0)
+            {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+void A_func()
+{
+    int n=2,sum=0,c=0;
+    
+    while(c<g_N)
+    {
+       if(is_prime(n))
+       {
+           sum+=n;
+           c++;
+       }
+       n++;
+    }
+    
+    printf("The sum of first %d prime numbers : %d\n",g_N,sum);
+   
+}
+
+void B_func()
+{
+    time_t start_time = time(NULL);
+    time_t begin=start_time;
+    while(difftime(time(NULL),begin)<100)
+    {
+        printf("Thread B is running successfully\n");
+        sleep(2);
+    }
+    time_t end_time = time(NULL);
+    printf("B_func is executed in duration : %ld\n",end_time - start_time);
+   
+    
+}
+
+void C_func()
+{
+    time_t start_time = time(NULL);
+    time_t begin=start_time;
+    while(difftime(time(NULL),begin)<100)
+    {
+        printf("Thread C is running successfully\n");
+        sleep(3);
+    }
+    time_t end_time = time(NULL);
+    printf("C_func is executed in duration : %ld\n",end_time - start_time);
+   
+    
+}
+
+int main()
+{
+    signal(SIGINT,handle_int);
+    
+    printf("Enter the value of N ");
+    scanf("%d",&g_N);
+    A_func();
+    B_func();
+    C_func();   
+    
+   
+    
+    return 0;
+}
